0d06d5423f51b519130eb31165c74ae27fa7b29f
Prince-Kid <mucyoprinc12@gmail.com>
2024-04-13 23:08:45 +0200

    Adding Cors

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,37 @@ import userRoute from "./Routes/User";
 import blogRoute from "./Routes/BlogRoute";
 import messageRoute from "./Routes/messageRoute";
 import upload from "./cloudinary/multer";
+import cors from "cors"
+import subRoute from "./Routes/Subscribe";
+import dotenv from "dotenv"
+
+dotenv.config()
 const app = express()
-mongoose.connect("mongodb://localhost:27017/My-brand").then(()=>{
+const MONGO_URL : any = process.env.MONGO_URL
+
+/************ Connecting Mongo Db***************/
+
+mongoose.connect(MONGO_URL).then(()=>{
     console.log("Db Connected !!")
 }).catch((error)=>{
     console.log(error.message);
 })
+const corsOptions = {
+    origin: "*", 
+    methods: "GET,POST,PUT,PATCH,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+};
 
+/************ Routes ***************/
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(upload.single('cover'))
 app.use("/auth",userRoute)
 app.use("/blog",blogRoute)
 app.use("/msg",messageRoute)
+app.use("/subscribe",subRoute)
+
 app.listen(5000,()=>{
     console.log("server running on port 5000")
 })
