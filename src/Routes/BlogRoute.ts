ecab6bd1ad292aad7b47804649dbe60619aea7bd
Prince-Kid <mucyoprinc12@gmail.com>
2024-04-13 23:06:52 +0200

    Fx-Routes

--- src/Routes/BlogRoute.ts
+++ src/Routes/BlogRoute.ts
@@ -1,14 +1,18 @@
 import  express  from "express";
-import { createBlog, getBlog ,getAllBlogs ,updateBlog,deleteBlog} from "../contollers/BlogsController";
+import { createBlog, getBlog ,getAllBlogs ,updateBlog,deleteBlog, blogComments, viewComment, addLike} from "../contollers/BlogsController";
+import { verifyToken } from "../middleware/verifyToken";
 
 
 const route = express.Router()
 
-route.post("/create",createBlog)
+route.post("/create",verifyToken,createBlog)
 route.get("/getBlog/:id",getBlog)
 route.get("/all",getAllBlogs)
-route.put("/update/:id",updateBlog)
-route.delete("/delete/:id",deleteBlog)
+route.put("/update/:id",verifyToken,updateBlog)
+route.delete("/delete/:id",verifyToken,deleteBlog)
+route.post("/comment/:id",blogComments)
+route.get("/viewcomment/:id",viewComment)
+route.post("/like/:id",addLike)
 
 const blogRoute = module.exports = route
 export default blogRoute
--- src/Routes/messageRoute.ts
+++ src/Routes/messageRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { userMessage } from "../contollers/message";
+
+import { userMessage, viewMessages } from "../contollers/message";
 
 
 const route = express.Router()
 
 route.post("/message",userMessage)
-
+route.get("/feedback",viewMessages)
 const messageRoute = module.exports = route
 export default messageRoute
